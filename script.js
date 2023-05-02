0f0aa3d3bf8c9d0329eaee6ac25d6eb387fcf952
Kurban Ramazanov <kurban.ramazanovv@gmail.com>
2023-05-02 07:31:37 +0300

    choose animation

---
+++ script.js
@@ -0,0 +1,89 @@
+const canvas = document.getElementById("canvas1");
+const dropdown = document.getElementById("animations");
+let chooseState = "Idle";
+
+const ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = canvas.width = 120;
+const CANVAS_HEIGHT = canvas.height = 130;
+
+const playerImage = new Image();
+playerImage.src = `Idle.png`;
+const spriteWidth = 128;
+const spriteHeight = 150;
+let gameFrame = 0;
+const staggerFrames = 13;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: "Walk",
+        frames: 7,
+        img: "Walk.png",
+    },
+    {
+        name: "Idle",
+        frames: 7,
+        img: "Idle.png",
+    },
+    {
+        name: "Run",
+        frames: 7,
+        img: "Run.png",
+    },
+    {
+        name: "Attack",
+        frames: 3,
+        img: "Attack.png",
+    },
+    {
+        name: "Dead",
+        frames: 5,
+        img: "Dead.png",
+    },
+    {
+        name: "Grenade",
+        frames: 9,
+        img: "grenade.png",
+    },
+    {
+        name: "Recharge",
+        frames: 13,
+        img: "grenade.png",
+    },
+    {
+        name: "Shot_1",
+        frames: 4,
+        img: "Shot_1.png",
+    },
+    {
+        name: "Shot_2",
+        frames: 4,
+        img: "Shot_2.png",
+    },
+];
+animationStates.forEach((state) => {
+    let frames = {
+        loc: [],
+    }
+    for (let j = 0; j < state.frames; j++) {
+        let posX = j * spriteWidth;
+        frames.loc.push(posX);
+    }
+    spriteAnimations[state.name] = frames;
+});
+
+dropdown.addEventListener("change", e => {
+    chooseState = e.target.value;
+    console.log(chooseState);
+    playerImage.src = `${chooseState}.png`;
+});
+console.log(spriteAnimations[chooseState].loc.length);
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[chooseState].loc.length;
+    let frameX = spriteWidth * position
+    ctx.drawImage(playerImage, frameX, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+animate();
